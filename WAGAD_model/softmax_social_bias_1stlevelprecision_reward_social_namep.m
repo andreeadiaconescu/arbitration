886e07649acfc7d0b7e1269a48dab43212b8cb3c
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2018-11-01 14:13:34 +0100

    Update response models to exclude the volatility aspect of the mapping.

--- WAGAD_model/softmax_social_bias_1stlevelprecision_reward_social_namep.m
+++ WAGAD_model/softmax_social_bias_1stlevelprecision_reward_social_namep.m
@@ -12,4 +12,29 @@ pstruct = struct;
 pstruct.ze  = pvec(1);
 pstruct.be_ch = pvec(2);
 pstruct.be_wager  = pvec(3);
+
+pvec    = NaN(1,length(ptrans));
+pstruct = struct;
+
+pvec(1)     = ptrans(1);         % be0
+pstruct.be0 = pvec(1);
+pvec(2)     = ptrans(2);         % be1
+pstruct.be1 = pvec(2);
+pvec(3)     = ptrans(3);         % be2
+pstruct.be2 = pvec(3);
+pvec(4)     = ptrans(4);         % be3
+pstruct.be3 = pvec(4);
+pvec(5)     = ptrans(5);         % be4
+pstruct.be4 = pvec(5);
+pvec(6)     = ptrans(6);         % be5
+pstruct.be5 = pvec(6);
+pvec(7)     = ptrans(7);         % be6
+pstruct.be6 = pvec(7);
+pvec(8)     = exp(ptrans(8));    % ze
+pstruct.ze  = pvec(8);
+pvec(9)     = exp(ptrans(9));    % be_ch
+pstruct.be_ch  = pvec(9);
+pvec(10)     = exp(ptrans(10));    % be_wager
+pstruct.be_wager  = pvec(10);
+
 return;
