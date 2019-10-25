d18cf217b218a1a5b368c02b4b787284df8a32b9
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2019-10-25 09:12:35 +0200

    Different approach for plotting the probe results.

--- PreprocessingAnalysis/wagad_analyze_probe.m
+++ PreprocessingAnalysis/wagad_analyze_probe.m
@@ -7,7 +7,7 @@ function wagad_analyze_probe(iSubjectArray)
 % the size of the Mu1hatMatrix: (nSubjects,nProbeQuestions)
 
 % Sigle-subject level GLM where Y represents the behavioural probe answers are x
-% and X are the Mu1hat_Advice Values from the time the Probe was shown 
+% and X are the Mu1hat_Advice Values from the time the Probe was shown
 % Andreea Diaconescu July 2012/2019
 
 % for WAGAD_0006, no physlogs were recorded
@@ -30,16 +30,15 @@ subjectIds = iSubjectArray';
 variables_wager = cell(nSubjects, 1); % 16 is the number of nonmodel-based variables
 behaviour_wager = cell(nSubjects, 1); % 16 is the number of nonmodel-based variables
 
-figure;
 
 for iSubject = 1:nSubjects
     iSubj = iSubjectArray(iSubject);
     paths = get_paths_wagad(iSubj);
     tmp = load(paths.winningModel,'est','-mat'); % Select the winning model only;
     
-    mu1hatAdviceSelected        = [tmp.est.traj.muhat_a(1,1) tmp.est.traj.muhat_a(14,1),...
-                                   tmp.est.traj.muhat_a(49,1) tmp.est.traj.muhat_a(73,1),...
-                                   tmp.est.traj.muhat_a(110,1)];
+    mu1hatAdviceSelected        = [tmp.est.traj.muhat_a(2,1) tmp.est.traj.muhat_a(14,1),...
+        tmp.est.traj.muhat_a(49,1) tmp.est.traj.muhat_a(73,1),...
+        tmp.est.traj.muhat_a(110,1)];
     variables_wager{iSubject,1} = mu1hatAdviceSelected;
 end
 
@@ -63,13 +62,75 @@ resultsProbe.df     = STATS.df;
 
 disp(resultsProbe);
 
-figure;for s=1:size(y,1), subplot(7,7,s); plot(x(s,:)', y(s,:)','*'); 
-    hold all; plot(0:0.01:1, mybeta(s,2)+mybeta(s,1)*[0:0.01:1]');
-    axis([-0.05 1.05 -0.05 1.05]);
-    xlabel('$$\hat{\mu}_1$$', 'Interpreter', 'Latex');
-    ylabel('category'); 
-    title(sprintf('WAGAD %d',iSubjectArray(s)));
+
+% Get probe structure
+random              = [1 0, ...
+                       0 0 0];
+helpful             = [0 1, ...
+                       1 0 0];
+misleading          = [0 0, ...
+                       0 1 1];
+
+figure;
+for s=1:size(y,1)   
+    selectedChoice           = x(s,:);
+    selectedModelReadout     = y(s,:);
+    actualProbeRandom     = mean(selectedChoice(:,logical(random)));
+    actualProbeHelpful    = mean(selectedChoice(:,logical(helpful)));
+    actualProbeMisleading = mean(selectedChoice(:,logical(misleading)));
+    
+    predictedProbeRandom     = mean(selectedModelReadout(:,logical(random)));
+    predictedProbeHelpful    = mean(selectedModelReadout(:,logical(helpful)));
+    predictedProbeMisleading = mean(selectedModelReadout(:,logical(misleading)));
+    
+    par{s,1} = actualProbeRandom;
+    par{s,2} = actualProbeHelpful;
+    par{s,3} = actualProbeMisleading;
+    
+    par{s,4} = predictedProbeRandom;
+    par{s,5} = predictedProbeHelpful;
+    par{s,6} = predictedProbeMisleading;
 end
+
+
+%% plot them
+nPhases = 3;
+colourArray    = {'g','r','c'};
+yLabelArray    = {'Initial','Stable','Volatile'};
+xLimitArray    = {[0 1], [0 1],[0 1]};
+yLimitArray    = {[-Inf Inf], [-Inf Inf],[-Inf Inf]};
+
+variables      = cell2mat(par);
+
+% The first phase is essentially the first trial; here the model predicts
+% 0.5 because that this the prior for the initial value of advice accuracy.
+
+for iProbe = 1:nPhases
+    subplot(2,2,iProbe);
+    X = variables(:,iProbe);
+    Y = variables(:,iProbe + nPhases);
+    
+    regressionMatrix       = [X ones(size(X))];
+    B                      = regressionMatrix\Y;
+    [R,P]                  = corrcoef(X,Y);
+    
+    pValueArray(iProbe)        = P(1,2);
+    slopeArray(iProbe)         = R(1,2);
+    
+    scatter(X, Y, [],'MarkerEdgeColor',[0 .5 .5],...
+        'MarkerFaceColor',colourArray{iProbe},...
+        'LineWidth',2);
+    hold all;
+    plot(X, B(1)*X+B(2), '-k');
+    axis square;
+    title(sprintf(yLabelArray{iProbe}));
+    xlim(xLimitArray{iProbe});
+    ylim(yLimitArray{iProbe});
+    xlabel(sprintf('Mean: Actual Rating'));
+    ylabel(sprintf('Mean: Predicted Rating)'));
+    set(gca,'FontName','Constantia','FontSize',30);
+end
+
 
 
 end
